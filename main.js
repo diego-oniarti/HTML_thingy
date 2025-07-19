82109906a3d4a7c9a12c42415d9d4c0a7dbaec25
Diego <69463084+diego-oniarti@users.noreply.github.com>
2025-07-19 16:45:32 +0200

    Added a src folder

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-#!/usr/bin/env Node
+#!/usr/bin/env node
 
-const {XMLParser} = require("fast-xml-parser");
-const {parse} = require("node-html-parser");
-const fs = require("node:fs")
-const path = require("node:path")
-const { ArgumentParser } = require("argparse");
-const process = require("node:process");
+import { XMLParser } from "fast-xml-parser";
+import { parse } from "node-html-parser";
+import { readFileSync, readdirSync, writeFileSync, existsSync, mkdirSync, lstatSync, copyFileSync } from "node:fs";
+import { join } from "node:path";
+import { ArgumentParser } from "argparse";
+import { cwd } from "node:process";
 
 const  parser = new ArgumentParser({
     description: "My HTML thingy"
 });
 parser.add_argument('-c', '--components', {default:'Components', help:'The name of the components directory'});
 parser.add_argument('-o', '--out', {default:'out', help:'The name of the output directory'})
+parser.add_argument('-s', '--src', {default:'src', help:'The name of the source directory'})
 const args = parser.parse_args();
 
 const COMPS_FOLDER = args.components;
 const OUT_FOLDER = args.out;
-const CWD = process.cwd();
+const SRC_FOLDER = args.src;
+const CWD = cwd();
 
-const options = {
+const parserXML = new XMLParser({
     ignoreAttributes:false,
     allowBooleanAttributes:true
-};
-const parserXML = new XMLParser(options);
+});
 const parseHTML = parse;
 
 /**
@@ -35,7 +36,7 @@ class Component {
      * @param {String} file_path - The path to the Component file
      */
     constructor(file_path) {
-        const file_contents = fs.readFileSync(file_path);
+        const file_contents = readFileSync(file_path);
         const XMLdata = parserXML.parse(file_contents).Element;
 
         this.name = XMLdata.Name;
@@ -44,20 +45,20 @@ class Component {
     }
 }
 
-/** @type {Map<String,Component>} */
+/** @type {Map<String, Component>} */
 const components = new Map();
-/** @type {String[]} */
-const component_names = [];
+/** @type {Set<String>} */
+const component_names = new Set();
 
 function leggi_componenti() {
-    for (let file_name of fs.readdirSync(path.join(CWD, COMPS_FOLDER))) {
-        const file_path = path.join(CWD, COMPS_FOLDER, file_name)
+    for (let file_name of readdirSync(join(CWD, COMPS_FOLDER))) {
+        const file_path = join(CWD, COMPS_FOLDER, file_name)
 
         const component = new Component(file_path);
         console.log(`Created component ${component.name}`);
 
         components.set(component.name, component);
-        component_names.push(component.name)
+        component_names.add(component.name)
     }
 }
 
@@ -79,39 +80,7 @@ function populate_template(name, params) {
 }
 
 function convert_file(file_path) {
-    // faccio l'index ora poi farò tutta una cartella 
-    const html_data = parseHTML(fs.readFileSync(path.join(CWD, file_path)));
-
-    /** @type {HTMLElement[]} */
-    // const stack = [html_data.childNodes[0]];
-    // while (stack.length>0) {
-    //     const corrente = stack.pop();
-    //     if (component_names.includes(corrente.rawTagName)) {
-    //         // substitute
-    //         const element_name = corrente.rawTagName;
-    //         const actual_params_str = corrente.toString();
-
-    //         // const actual_para_XML = parserXML.parse(actual_params_str);
-    //         const actual_params = {};
-    //         for (let param_name of components.get(element_name).parameters) {
-    //             actual_params[param_name] = actual_params_str.match(new RegExp(`<${param_name}>([\\S\\s]*)<\/${param_name}>`))[1];
-    //         }
-
-    //         const new_elem = parseHTML(
-    //             populate_template(
-    //                 element_name, 
-    //                 actual_params
-    //             )
-    //         );
-
-    //         corrente.replaceWith(new_elem);
-    //         stack.push(new_elem);
-    //     }else{
-    //         for (let child of corrente.childNodes) {
-    //             stack.push(child)
-    //         }
-    //     }
-    // }
+    const html_data = parseHTML(readFileSync(join(CWD, SRC_FOLDER, file_path)));
 
     const stack = [html_data.childNodes[0]];
     const stack_reverse = [];
@@ -123,8 +92,8 @@ function convert_file(file_path) {
         }
     }
     while (stack_reverse.length>0) {
-        corrente = stack_reverse.pop();
-        if (component_names.includes(corrente.rawTagName)) {
+        const corrente = stack_reverse.pop();
+        if (component_names.has(corrente.rawTagName)) {
             // substitute
             const element_name = corrente.rawTagName;
             const actual_params_str = corrente.toString();
@@ -146,40 +115,39 @@ function convert_file(file_path) {
         }
     }
 
-    fs.writeFileSync(path.join(CWD, OUT_FOLDER, file_path), html_data.toString());
+    writeFileSync(join(CWD, OUT_FOLDER, file_path), html_data.toString());
 }
 
 function main() {
     leggi_componenti();
-    if (!fs.existsSync(OUT_FOLDER)) fs.mkdirSync(path.join(CWD,OUT_FOLDER))
-
-    const full_components_path = path.join(CWD,COMPS_FOLDER);
-    const full_out_path = path.join(CWD,OUT_FOLDER);
+    if (!existsSync(OUT_FOLDER)) mkdirSync(join(CWD, OUT_FOLDER))
 
     const stack = ['.'];
+    const SRC_BASE = join(CWD, SRC_FOLDER);
     while (stack.length>0) {
         const corrente = stack.pop();
 
-        for (let child_name of fs.readdirSync(path.join(CWD, corrente))) {
-            const full_child_path = path.join(CWD,corrente,child_name)
-            const relative_child_path = path.join(corrente,child_name);
+        for (let child_name of readdirSync(join(SRC_BASE, corrente))) {
+            const full_child_path = join(SRC_BASE, corrente, child_name)
+            const relative_child_path = join(corrente, child_name);
 
-            if (full_child_path==full_components_path || full_child_path==full_out_path) continue;
-
-            const lstat = fs.lstatSync(full_child_path);
+            const lstat = lstatSync(full_child_path);
 
             if (lstat.isDirectory()) {
                 stack.push(relative_child_path);
-                fs.mkdirSync(path.join(CWD,OUT_FOLDER,corrente,child_name));
+                const newDir = join(CWD, OUT_FOLDER, relative_child_path);
+                if (!existsSync(newDir)) {
+                    mkdirSync(newDir);
+                }
             }
 
             if (lstat.isFile()) {
                 if (child_name.endsWith(".html")) {
-                    convert_file(relative_child_path)
+                    convert_file(join(relative_child_path));
                 }else{
-                    fs.copyFileSync(
+                    copyFileSync(
                         full_child_path,
-                        path.join(CWD,OUT_FOLDER,corrente,child_name)
+                        join(CWD, OUT_FOLDER, relative_child_path)
                     );
                 }
             }
